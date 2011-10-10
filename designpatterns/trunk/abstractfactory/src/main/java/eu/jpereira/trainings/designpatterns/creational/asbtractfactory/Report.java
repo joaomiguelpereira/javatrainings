b59ff0ecc3ac0b1b66115f7591d9b10ac02e1825
joaomiguel.pereira@gmail.com <joaomiguel.pereira@gmail.com@737e350e-228b-7272-34ae-3263785c5075>
2011-10-10 20:59:56 +0000

    Initial import.

---
+++ designpatterns/trunk/abstractfactory/src/main/java/eu/jpereira/trainings/designpatterns/creational/asbtractfactory/Report.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2011 Joao Miguel Pereira
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eu.jpereira.trainings.designpatterns.creational.asbtractfactory;
+
+import eu.jpereira.trainings.designpatterns.creational.asbtractfactory.json.JSONReportBody;
+import eu.jpereira.trainings.designpatterns.creational.asbtractfactory.json.JSONReportFooter;
+import eu.jpereira.trainings.designpatterns.creational.asbtractfactory.json.JSONReportHeader;
+import eu.jpereira.trainings.designpatterns.creational.asbtractfactory.xml.XMLReportBody;
+import eu.jpereira.trainings.designpatterns.creational.asbtractfactory.xml.XMLReportFooter;
+import eu.jpereira.trainings.designpatterns.creational.asbtractfactory.xml.XMLReportHeader;
+
+public class Report {
+
+	private String reportContent;
+	private ReportBody body;
+	private ReportFooter footer;
+	private ReportHeader header;
+	private String reportType;
+	
+
+	
+	
+	/**
+	 * @param string
+	 */
+	public Report(String string) {
+		this.reportType = string;
+		if ( reportType.equals("JSON")) {
+			//to compose Report with JSON objects
+			this.setBody(new JSONReportBody());
+			this.setFooter(new JSONReportFooter());
+			this.setHeader(new JSONReportHeader());
+		} else {
+			this.setFooter(new XMLReportFooter());
+			this.setHeader(new XMLReportHeader());
+			this.setBody(new XMLReportBody());
+		}
+	}
+
+
+	public void setBody(ReportBody body) {
+		this.body = body;
+
+	}
+
+	
+	public void setFooter(ReportFooter footer) {
+		this.footer = footer;
+
+	}
+
+	
+	public void setHeader(ReportHeader header) {
+		this.header = header;
+	}
+
+	public void setReportContent(String reportContent) {
+		this.reportContent = reportContent;
+	}
+
+
+	public String getReportContent() {
+		return reportContent;
+	}
+
+
+	public ReportBody getBody() {
+		return body;
+	}
+
+
+	public ReportFooter getFooter() {
+		return footer;
+	}
+
+
+	public ReportHeader getHeader() {
+		return header;
+	}
+
+	
+}
